9f6f054ac80e233044f900b31c5abd30059fbe68
saripagurek <saripvm@gmail.com>
2015-05-23 15:44:23 -0700

    convert a word

---
+++ piglatin.rb
@@ -0,0 +1,15 @@
+puts "Type a word"
+input = gets.chomp
+
+if input [0]=="a" or input [0]=="e" or input [0]== "i" or input [0]== "o" or input [0]== "u" or input [0]== "y" or input [0]== "A" or input [0]=="E" or input [0]== "I" or input [0]== "O" or input [0]== "U" or input [0]== "Y"
+  puts input + "way"
+else
+  puts input[1..-1]    + input[0] + "ay"
+
+end
+
+
+#this is all david's fault
+#i will delete this because of the weird monstrosity that is david.
+#orange tangarine monkeys
+gets
